81e812beb40973d6a4e6ff5c70232fafb7dd600b
Jammonjones <wjssaan@gmail.com>
2024-02-15 14:04:40 -0700

    got rid of everything other than components and basic styles

--- NextJS Chat Application/src/app/components/MessageList.tsx
+++ NextJS Chat Application/src/app/components/MessageList.tsx
@@ -3,14 +3,11 @@
 
 import styles from './messageList.module.css'
 
-export default function MessageList({chatList}){
+export default function MessageList(){
     return(
         <>
         <main className={styles.main}>
             <ul>
-                {chatList.map((message)=>{
-                    return <li key={Date.now}>{message.message}</li>
-                })}
             </ul>
         </main>
         </>
